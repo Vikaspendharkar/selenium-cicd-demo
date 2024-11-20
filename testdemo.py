42748cd529f997d5ceeadd1948ef8d31ee65a61e
Vikaspendharkar <105772769+Vikaspendharkar@users.noreply.github.com>
2024-11-20 15:11:25 +0530

    Update testdemo.py

--- testdemo.py
+++ testdemo.py
@@ -2,23 +2,21 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
-import pytest
 
-@pytest.fixture
-def driver():
-    # Setup WebDriver
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-    yield driver
-    # Quit the WebDriver after the test
-    driver.quit()
+
+
+# Setup WebDriver
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
     
 
 
-def test_google_search(driver):
+def test_google_search():
     driver.get("https://www.google.com")
     assert "Google" in driver.title
     search_box = driver.find_element(By.NAME, "q")
     search_box.send_keys("Selenium Python")
     search_box.submit()
-    assert "Selenium Python" in driver.page_source
+    assert "Selenium Python" in driver.title
 
+test_google_search()
